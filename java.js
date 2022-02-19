66df48e2c3015de61ac35b37380fae0e9d539f5b
Nuclear One <ajithsnair10@gmail.com>
2022-02-19 09:40:44 +0530

    error

--- java.js
+++ java.js
@@ -2,6 +2,7 @@ let form=document.getElementById('addForm');
 let itemlist=document.getElementById('items');
 let sele=document.querySelectorAll('li')
 let filter=document.getElementById('filter');
+let d=1;
 
 //submit function 
 form.addEventListener('submit' , Takeitem);
@@ -16,7 +17,7 @@ for(let i=0;i<sele.length;i++){
 
     let edbtn=document.createElement('button');
     edbtn.className='edit-button btn-danger btn-sm float-right edit';
-    edbtn.appendChild(document.createTextNode('Edit '));
+    edbtn.appendChild(document.createTextNode('Edit'));
     sele[i].appendChild(edbtn);
 }
 
@@ -28,6 +29,7 @@ function Takeitem(e){
     let li=document.createElement('li');
     li.className='list-group-item';
     li.appendChild(document.createTextNode(NewItem));
+
     //delete button
     let dlt=document.createElement('button');
     //add class to the button
@@ -44,6 +46,24 @@ function Takeitem(e){
     edbtn.appendChild(document.createTextNode('Edit'));
     li.appendChild(edbtn);
     
+
+//create Div
+    let newDiv=document.createElement('div')
+    newDiv.className='Div-class';
+    newDiv.id='div1';
+    //create text node
+    let newDivText=document.createTextNode(`description ${d}`)
+    d=d+1;
+    //add to div
+    newDiv.appendChild(newDivText);
+    //console.log(newDiv)
+    
+    //add element to the page
+    let M=document.querySelector(`ul .list-group-item`)
+    let TT=document.querySelector('form li');
+    //console.log(newDiv);
+    sele[j].insertBefore(newDiv,TT);
+
 }
 
 
@@ -76,3 +96,29 @@ function filterfn(e){
         }
     });
 }
+
+
+//Add discription to all items
+let j=0;
+while(j<=sele.length){
+//create Div
+let newDiv=document.createElement('div')
+newDiv.className='Description';
+newDiv.id='des1';
+//create text node
+let newDivText=document.createTextNode(`description ${d}`)
+d=d+1;
+//add to div
+newDiv.appendChild(newDivText);
+//console.log(newDiv)
+    
+//add element to the page
+let M=document.querySelector(`ul .list-group-item`);
+let TT=document.querySelector('form li');
+//console.log(newDiv);
+sele[j].insertBefore(newDiv,TT);
+j=j+1;
+}
+
+
+
